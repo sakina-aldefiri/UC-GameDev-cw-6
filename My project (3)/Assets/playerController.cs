41ef9dd6a1940e76ccfc1c80faa0178077548855
sakina-aldefiri <112488150+sakina-aldefiri@users.noreply.github.com>
2022-09-19 20:20:34 +0300

    done cw6

---
+++ My project (3)/Assets/playerController.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+
+    public float jumpForce;
+    bool isGrounded;
+    public float rad;
+    public LayerMask ground;
+    public Transform groundPos;
+
+    bool isRight = true;
+    SpriteRenderer sprite;
+
+    Animator anim;
+    string currAnim;
+    const string IDLE_ANIM = "idle";
+    const string WALK_ANIM = "walk";
+    const string JUMP_ANIM = "jump";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MovePlayer();
+        PlayerJump();
+    }
+    void MovePlayer()
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
+        if(isRight && Input.GetKey(KeyCode.A))
+        {
+            isRight = false;
+            sprite.flipX = true;
+        }
+        else if(!isRight && Input.GetKey(KeyCode.D))
+        {
+            isRight = true;
+            sprite.flipX = false;
+        }
+
+        if(rb.velocity.x != 0 && rb.velocity.y == 0)
+        {
+            PlayAnim(WALK_ANIM);
+        }
+        else if(rb.velocity.x == 0 && rb.velocity.y == 0)
+        {
+            PlayAnim(IDLE_ANIM);
+        }
+    }
+    void PlayerJump()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundPos.position, rad, ground);
+        if(Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            PlayAnim(JUMP_ANIM);
+        }
+    }
+    void PlayAnim(string nextAnim)
+    {
+        if(currAnim == nextAnim)
+        {
+            return;
+        }
+        anim.Play(nextAnim);
+        currAnim = nextAnim;
+    }    
+    
+}
